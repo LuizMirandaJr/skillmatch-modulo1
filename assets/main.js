658d8fd75b7f535bbc201df59fb040402d75dfe2
Luiz Miranda Jr <mirandajuniorsc@gmail.com>
2026-07-03 16:32:26 -0300

    feat: Inicio da criação da interface de usuário

---
+++ assets/main.js
@@ -0,0 +1,22 @@
+const habilidades = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "TypeScript",
+    "React",
+    "Vue",
+    "Angular",
+    "Node.js",
+    "Express",
+    "MongoDB",
+    "MySQL",
+    "Git",
+    "GitHub",
+    "Docker",
+    "Python",
+    "Java",
+    "C#",
+    "PHP"
+]
+
+console.log(habilidades)
