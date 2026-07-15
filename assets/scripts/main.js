94e672b1a6973061aeb714ee9691371ab73d7122
LuizMirandaJr <mirandajuniorsc@gmail.com>
2026-07-14 22:57:50 -0300

    feat: ajuste da função de mostrar vagas.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -71,14 +71,9 @@ async function carregarVagas() {
 
     function mostrarVagas() {
         const mostrarTitulo = document.getElementById("container-vagas")
-        const htmlVaga = document.getElementById("container-card")
+        const htmlVaga = document.getElementById("container-job")
 
-        mostrarTitulo.innerHTML = `
-        <div class="vagas-titulos">
-        <h2 id="titulo-h2"><i class="bi bi-stars icon-title"></i> Vagas para você</h2>
-        <p class="vagas-texto">Todas as vagas baseadas no seu perfil</p>
-        </div>
-        `
+        mostrarTitulo.classList.remove("hidden")
 
         htmlVaga.innerHTML = ""
         let renderizarLista = ""
@@ -86,27 +81,30 @@ async function carregarVagas() {
 
         vagasCarregadas.forEach(vaga => {
             renderizarLista += `
-            <div class="job-card" id="job-card">
-                    <div class="job-card-sup">
-                        <div class="job-card-img">
-                            <div class="logo-vaga">
-                                <img src="${vaga.logo}" alt="">
+            <div class="container-card" id="container-card">
+                    <div class="job-card" id="job-card">
+                        <div class="job-card-sup">
+                            <div class="job-card-img">
+                                <div class="logo-vaga">
+                                    <img src="${vaga.logo}" alt="">
+                                </div>
+                            </div>
+                            <div class="job-infos">
+                                <p id="empresa" class="empresa">${vaga.empresa}</p>
+                                <h3 id="cargo-vaga" class="cargo-vaga">${vaga.cargo}</h3>
+                                <div class="infos-contrato">
+                                    <span id="modelo" class="tag-vaga bg-modelo">${vaga.modelo}</span>
+                                    <span id="salario" class="tag-vaga bg-salario">${vaga.salario}</span>
+                                    <span id="contrato" class="tag-vaga bg-contrato">${vaga.contrato}</span>
+                                </div>
                             </div>
                         </div>
-                        <div class="job-infos">
-                            <p id="empresa" class="empresa">${vaga.empresa}</p>
-                            <h3 id="cargo-vaga" class="cargo-vaga">${vaga.cargo}</h3>
-                            <div class="modelo-salario">
-                                <span id="modelo" class="tag-vaga bg-modelo">${vaga.modelo}</span>
-                                <span id="salario" class="tag-vaga bg-salario">${vaga.salario}</span>
-                                <span id="contrato" class="tag-vaga bg-contrato">${vaga.contrato}</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="compatibilidade">
-                        <p id="compatibilidade" class="comp-texto">Compatibilidade</p>
-                        <div id="barra-bg" class="w3-round barra-bg barra">
-                            <div id="barra-txt" class="w3-container w3-round barra-cor barra-txt" style="width:25%">25%
+                        <div class="compatibilidade">
+                            <p id="compatibilidade" class="comp-texto">Compatibilidade</p>
+                            <div id="barra-bg" class="w3-round barra-bg barra">
+                                <div id="barra-txt" class="w3-container w3-round barra-cor barra-txt" style="width:25%">
+                                    25%
+                                </div>
                             </div>
                         </div>
                     </div>
